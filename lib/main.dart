5b5f1f04751744fa857a2b31c9c53bc78d4d1808
khuong <vankhuong240499@gmail.com>
2022-01-17 10:45:20 +0700

    Multiple Bloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => CounterProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CounterProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SettingProvider(),
+        )
+      ],
       child: const MyApp(),
     ),
   );
@@ -38,10 +45,28 @@ class CounterProvider extends ChangeNotifier {
     _counter++;
     notifyListeners();
   }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+class SettingProvider extends ChangeNotifier {
+  late String text = "INIT";
+  late Color color = Colors.red;
+
+  void changeText() {
+    if (text == 'Hello') {
+      text = "World";
+    } else {
+      text = "Hello";
+    }
+    notifyListeners();
+  }
+
+  void changeColor() {
+    if (color == Colors.red) {
+      color = Colors.blue.shade100;
+    } else {
+      color = Colors.red;
+    }
+    notifyListeners();
   }
 }
 
@@ -50,19 +75,44 @@ class CounterHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          context.watch<CounterProvider>().counter.toString(),
-          style: const TextStyle(fontSize: 20),
+    return Consumer<SettingProvider>(builder: (context, setting, child) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: setting.color,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          context.read<CounterProvider>().add();
-        },
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                "Change Color",
+                style: const TextStyle(fontSize: 20),
+              ),
+              Text(
+                "Change Text ${setting.text}",
+                style: const TextStyle(fontSize: 20),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<SettingProvider>().changeText();
+                },
+                child: Text("ChangeText"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<SettingProvider>().changeColor();
+                },
+                child: Text("ChangeColor"),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            context.read<CounterProvider>().add();
+          },
+        ),
+      );
+    });
   }
 }
